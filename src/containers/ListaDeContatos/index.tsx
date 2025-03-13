81f879ce67012da2d48cae0ce4adf06660bb0d64
felipe bartolomeu <felipebart1999@gmail.com>
2025-03-13 05:37:02 -0300

    redux configurado

--- src/containers/ListaDeContatos/index.tsx
+++ src/containers/ListaDeContatos/index.tsx
@@ -1,41 +1,26 @@
 import Contato from "../../components/Contato";
+import { useAppSelector } from "../../redux/hooks";
 import { Container } from "./styles";
 
-const contatos = [
-  {
-    nome: "Felipe",
-    tag: "familia",
-    email: "felipebart1999@gmail.com",
-    telefone: "81998602739",
-  },
-  {
-    nome: "Erik",
-    tag: "amigos",
-    email: "felipebart1999@gmail.com",
-    telefone: "81998602739",
-  },
-  {
-    nome: "Paula",
-    tag: "trabalho",
-    email: "felipebart1999@gmail.com",
-    telefone: "81998602739",
-  },
-];
-const ListaDeContatos = () => (
-  <Container>
-    <ul>
-      {contatos.map((t) => (
-        <li key={t.nome}>
-          <Contato
-            nome={t.nome}
-            tag={t.tag}
-            email={t.email}
-            telefone={t.telefone}
-          />
-        </li>
-      ))}
-    </ul>
-  </Container>
-);
+const ListaDeContatos = () => {
+  const contatos = useAppSelector((state) => state.contatos);
+
+  return (
+    <Container>
+      <ul>
+        {contatos.map((t) => (
+          <li key={t.nome}>
+            <Contato
+              nome={t.nome}
+              tag={t.tag}
+              email={t.email}
+              telefone={t.telefone}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
+};
 
 export default ListaDeContatos;
